301ca1cced6a32a5f3fa72fc29331aa8d414edad
Murilo Augusto Braun <113352904+braunNS@users.noreply.github.com>
2024-11-26 07:55:16 -0300

    fdawa

---
+++ pergunta.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Matemática',
+    'Qual o valor de X? 2X + 60 = 3X . 5',
+    'X = 12'
+)
+criaCartao(
+    'historia',
+    'Quem descobriu as américas e o Brasil respectivamente?',
+    'Cristóvão Colombo e Pedro Álvares Cabral'
+)
+criaCartao(
+    'Geografia',
+    'Em que continente fica Coreia do sul, Espanha e Colômbia?',
+    'Ásia, europa e América do sul'
+)
+criaCartao(
+    'Fisica',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'Newton'
+)
+criaCartao(
+    'Biologia',
+    'Qual a cor predominante na clorofila?',
+    'verde'
+)
+criaCartao(
+    'Química',
+    'O que é PH?',
+    'Medida de acidez de uma solução.'
+)
+criaCartao(
+    'Inglês',
+    'Transforme a frase em uma pergunta: you know.',
+    'do you know?'
+)
+criaCartao(
+    'Sociologia',
+    'O que é estratificação social?',
+    'Divisão da sociedade em camadas.'
+)
+criaCartao(
+    'Educação financeira',
+    'Qual o preço do dólar em reais?',
+    'R$5,51.'
+)
+criaCartao(
+    'Biotecnologia',
+    'O que é um organismo geneticamente modificado?',
+    'Um organismo com genes alterados.'
+)
+criaCartao(
+    'Matemática li',
+    'O que é uma variável?',
+    'É um nome para guardar uma informação.',
+)
+criaCartao(
+    'Projeto de vida',
+    'O que é projeto de vida?',
+    'É um plano para alcançar suas metas.'
+)
